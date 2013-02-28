48f850efee785da5ecca0f26e192531474416e2c
edemar <edemar@edemar-ubuntu.(none)>
2013-02-28 16:51:14 -0300

    Adicionando projeto Netbeans

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: edemar
+ *
+ * Created on 28 de Fevereiro de 2013, 16:47
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
